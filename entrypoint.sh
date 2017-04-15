3fb7f53dd05c22f8a1c86179f4c25a5073d75e78
Sergey Grankin <sgrankin@gmail.com>
2017-04-14 23:42:20 -0400

    allow $UID/$GID override; default to nobody

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -euo pipefail
-chown -R bitcoin:bitcoin /var/lib/bitcoin
-exec /sbin/chpst -u bitcoin:bitcoin /usr/bin/bitcoind \
+: ${UID:=$(id -u nobody)}
+: ${GID:=$(id -g nobody)}
+
+chown -R $UID:$GID /var/lib/bitcoin
+exec /sbin/chpst -u :$UID:$GID /usr/bin/bitcoind \
   -conf=/etc/bitcoin.conf \
   -datadir=/var/lib/bitcoin \
   -server \
